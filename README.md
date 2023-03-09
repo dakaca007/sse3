1f33dcb0e51c2260174195e92c2f52af208b4a52
Natapone Charsombut <natapone@gmail.com>
2023-03-09 21:34:26 +0700

    docker build hello world

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # docker_openai_service_flask
 Flask backend as a proxy for calling OpenAI API
+
+
+
+# -- Build --
+$ docker build -t flask_web_app .
+# Run image
+$ docker run --name flask_web_app -p 40002:40002 flask_web_app
---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+# import openai
+
+app = Flask(__name__)
+# openai.api_key = "YOUR_API_KEY"
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/openai')
+def openai_endpoint():
+    prompt = "Hello, OpenAI!"
+    # response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=10)
+    # return jsonify(response.choices[0].text)
+
+    return prompt
+
+if __name__ == '__main__':
+    # port = 5000
+    # host='0.0.0.0'
+    app.run(debug=True, host='0.0.0.0', port = 40002)
